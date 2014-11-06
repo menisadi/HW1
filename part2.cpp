c2782dadb03bceb50a96d20945a3e3a5bfcc73ba
meni <menitm@gmail.com>
2014-11-06 09:33:23 +0200

    just 1 cpp

---
+++ part2.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <iostream>
+#include <string>
+using namespace std;
+std::vector<std::string> split(char sentence[])
+{
+	int index =0;
+	std::vector<std::string> words;
+	std::string str(sentence);
+	std::string cutStr;
+	index = str.length();
+	while(index<=str.length()+1)
+	{
+		index = str.find_first_of(' ',1);
+		cutStr.append(str,0,index);
+		words.push_back(cutStr);
+		cutStr = str.substr(index+1,str.length());
+		str=cutStr;
+		cutStr="";
+	}
+	return words;
+}
+void printWords(std::vector<std::string> words)
+{
+	for(std::vector<string>::iterator it = words.begin(); it != words.end(); ++it)
+		    std::cout << *it << endl;
+}
+int main()
+{
+	char sentence[]="12345 abcd 1A 2B hh 77 99 00 2 12345 44789 11111 22222 44444 ";
+	printWords(split(sentence));
+    return 0;
+}
